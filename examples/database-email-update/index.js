da0df762cb39632998535906ecc5a921a1e7a62a
jeonghoHyeon-kontactic <jeonghohyeon@kontactic.com>
2022-09-21 18:49:24 +0900

    # 001 프로젝트 생성

--- examples/database-email-update/index.js
+++ examples/database-email-update/index.js
@@ -7,6 +7,14 @@
 
 ================================================================================ */
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 const { Client } = require("@notionhq/client")
 const dotenv = require("dotenv")
 const sendgridMail = require("@sendgrid/mail")
